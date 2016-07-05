4f32607347b4bd601601d3746c78f23c7f61ea77
dsky1990 <dutian1990@gmail.com>
2016-07-05 16:17:06 +0800

    add file path

--- gulpfile.js
+++ gulpfile.js
@@ -25,26 +25,38 @@ var postcss = require('gulp-postcss');
 var reporter = require('postcss-reporter');
 var syntax_scss = require('postcss-scss');
 var stylelint = require('stylelint');
+// file path
+var buildImg = 'build/images/*';
+var buildImgSrc = 'build/images/';
+var buildSass = 'build/sass/*.scss';
+var buildSassSrc = 'build/sass/';
+var buildIcon = 'build/icon/*';
+var buildCss = 'build/css/*.css';
+var buildCssSrc = 'build/css';
+var buildJs = 'build/js/*.js';
+var distImgSrc = 'dist/images';
+var distCssSrc = 'dist/css';
+var distJsSrc = 'dist/js';
 //css sprite
 gulp.task('css-sprite', function(){
-  return gulp.src('build/icon/*')
+  return gulp.src(buildIcon)
       .pipe(spritesmith({
         imgName: 'sprite.png',
         cssName: 'icon.css',
         imgPath:'../images/sprite.png'
       }))
-      .pipe(gulpif('*.png', gulp.dest('build/images/'), gulp.dest('build/sass/')));
+      .pipe(gulpif('*.png', gulp.dest(buildImgSrc), gulp.dest(buildSassSrc)));
 });
 //imagemin
 gulp.task('imagemin',['css-sprite'], function(){
-  return gulp.src('build/images/*')
-      .pipe(changed('build/images/*'))
+  return gulp.src(buildImg)
+      .pipe(changed(buildImg))
       .pipe(imagemin({
         progressive: true,
         interlaced: true,
         use: [pngquant()]
       }))
-      .pipe(gulp.dest('dist/images'));
+      .pipe(gulp.dest(distImgSrc));
 });
 //sass lint
 gulp.task("scss-lint", function() {
@@ -54,13 +66,13 @@ gulp.task("scss-lint", function() {
       clearMessages: true
     })
   ];
-  return gulp.src('build/sass/*.scss')
+  return gulp.src(buildSass)
       .pipe(postcss(processors, {syntax: syntax_scss}));
 });
 //sass to css
 gulp.task('sass-to-css', ['scss-lint'], function(){
-  return gulp.src('build/sass/*.scss')
-      .pipe(changed('build/sass/*'))
+  return gulp.src(buildSass)
+      .pipe(changed(buildSass))
       .pipe(sourceMaps.init())
       .pipe(sass().on('error', function (e) {
         console.error(e.message);
@@ -70,29 +82,22 @@ gulp.task('sass-to-css', ['scss-lint'], function(){
         cascade: false
       }))
       .pipe(sourceMaps.write('../../dist/css/maps'))
-      .pipe(gulp.dest('build/css'));
-});
-
-//concat
-gulp.task('concat', function() {
-  return gulp.src('build/sass/*.scss')
-    .pipe(concat('style.scss'))
-    .pipe(gulp.dest('build/sass'));
+      .pipe(gulp.dest());
 });
 
 //css minify
 gulp.task('minify-css', function() {
-  return gulp.src('build/css/*.css')
-      .pipe(changed('build/css/*.css'))
+  return gulp.src(buildCss)
+      .pipe(changed(buildCss))
       // .pipe(assetRev())
       .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(concat('style.css'))
-      .pipe(gulp.dest('dist/css'));
+      .pipe(gulp.dest(distCssSrc));
 });
 //js lint
 gulp.task('eslint', function () {
-  return gulp.src(['build/js/*.js'])
-      .pipe(changed('build/js/*.js'))
+  return gulp.src(buildJs)
+      .pipe(changed(buildJs))
       .pipe(eslint())
       .pipe(eslint.format())
       .pipe(eslint.failAfterError());
@@ -100,9 +105,9 @@ gulp.task('eslint', function () {
 // js minify
 gulp.task('jscompress',['eslint'], function (cb) {
   pump([
-        gulp.src('build/js/*.js'),
+        gulp.src(buildJs),
         uglify(),
-        gulp.dest('dist/js')
+        gulp.dest(distJsSrc)
       ],
       cb
   );
@@ -110,27 +115,27 @@ gulp.task('jscompress',['eslint'], function (cb) {
 
 // watch icon
 gulp.task('watch-icon', function (done) {
-  gulp.watch('./build/icon/*', ['css-sprite'])
+  gulp.watch(buildIcon, ['css-sprite'])
       .on('end', done);
 });
 // watch img
 gulp.task('watch-img', function (done) {
-  gulp.watch('./build/images/*', ['imagemin'])
+  gulp.watch(buildImg, ['imagemin'])
       .on('end', done);
 });
 //watch sass
 gulp.task('watch-sass', function (done) {
-  gulp.watch('./build/sass/*', ['sass-to-css'])
+  gulp.watch(buildSass, ['sass-to-css'])
       .on('end', done);
 });
 //watch css
 gulp.task('watch-css', function (done) {
-  gulp.watch('./build/css/*', ['minify-css'])
+  gulp.watch(buildCss, ['minify-css'])
       .on('end', done);
 });
 //watch js
 gulp.task('watch-js', function (done) {
-  gulp.watch('./build/js/*.js', ['jscompress'])
+  gulp.watch(buildJs, ['jscompress'])
       .on('end', done);
 });
 //前端开发版
